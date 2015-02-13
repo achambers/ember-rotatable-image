47ce32721eea287f9ee5ac9ba75699a2fc43c69a
Aaron Chambers <achambers@gmail.com>
2015-02-13 20:49:15 +0000

    Rotating image on click

--- app/components/image-tag.js
+++ app/components/image-tag.js
@@ -4,19 +4,39 @@ export default Em.Component.extend({
   tagName: ['img'],
   classNames: ['rotatable-image'],
   attributeBindings: ['src', 'alt', 'style'],
+  degrees: 0,
+  isOriginalOrientation: true,
 
   onDidInsertElement: function() {
     var element = this.get('element');
 
-    element.onload = function() {
-      Em.run(this, function() {
-        console.log(element.offsetWidth);
-        console.log(element.offsetHeight);
-      });
-    }
+    //element.onload = function() {
+      //Em.run(this, function() {
+        //console.log(element.offsetWidth);
+        //console.log(element.offsetHeight);
+      //});
+    //}
   }.on('didInsertElement'),
 
-  style: Em.computed(function() {
-    return 'transform: rotate(0deg) scale(1);';
-  })
+  style: Em.computed('degrees', function() {
+    var degrees = this.get('degrees');
+
+    return 'transform: rotate(' + degrees + 'deg) scale(1);';
+  }),
+
+  click: function() {
+    this._rotate.apply(this);
+  },
+
+  _rotate: function() {
+    var amountToRotate = this._amountToRotate.apply(this);
+
+    this.incrementProperty('degrees', amountToRotate);
+  },
+
+  _amountToRotate: function() {
+    var degrees = this.get('degrees');
+
+    return degrees === 270 ? -270 : 90;
+  }
 });
