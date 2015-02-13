8ae38a689d6a0d0e15c5c483d7d1f966f5e71cec
Aaron Chambers <achambers@gmail.com>
2015-02-13 20:42:40 +0000

    Added img style attribute bindings

--- app/components/image-tag.js
+++ app/components/image-tag.js
@@ -1,7 +1,22 @@
-import Ember from 'ember';
+import Em from 'ember';
 
-export default Ember.Component.extend({
+export default Em.Component.extend({
   tagName: ['img'],
-  classNames: 'rotatable-image',
-  attributeBindings: ['src', 'alt']
+  classNames: ['rotatable-image'],
+  attributeBindings: ['src', 'alt', 'style'],
+
+  onDidInsertElement: function() {
+    var element = this.get('element');
+
+    element.onload = function() {
+      Em.run(this, function() {
+        console.log(element.offsetWidth);
+        console.log(element.offsetHeight);
+      });
+    }
+  }.on('didInsertElement'),
+
+  style: Em.computed(function() {
+    return 'transform: rotate(0deg) scale(1);';
+  })
 });
